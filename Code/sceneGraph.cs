da60028378c3b0e6089ad5540f26251867adb8f7
PJvGrol <pjvgrol@xs4all.nl>
2016-06-07 11:28:12 +0200

    Fixed indentation, added sceneGraph class

---
+++ Code/sceneGraph.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace template_P3
+{
+    class SceneGraph
+    {
+        static void Render(Matrix3 cam)
+        {
+
+        }
+    }
+}
